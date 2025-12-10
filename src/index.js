ae532e27d7faeeec90fd7bdfa5c33c190a7c244a
AttikusFinch <amirmistikad@gmail.com>
2025-12-11 00:54:20 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-const { JSDOM } = require("jsdom");
+const { JSDOM, VirtualConsole } = require("jsdom");
 
 module.exports = async (animationData, opts, frameNumber) => {
+    const virtualConsole = new VirtualConsole();
+    virtualConsole.sendTo(console);
 	const { window } = new JSDOM("<!DOCTYPE html><body></body>", {
-		pretendToBeVisual: true
+		pretendToBeVisual: true,
+        virtualConsole
 	});
 
 	const { document, navigator } = window;
@@ -11,6 +14,63 @@ module.exports = async (animationData, opts, frameNumber) => {
 	global.window = window;
 	global.navigator = navigator;
 	global.document = document;
+    
+    // Polyfill requestAnimationFrame for lottie-web
+    if (!global.requestAnimationFrame) {
+        global.requestAnimationFrame = (callback) => setTimeout(callback, 1000 / 60);
+        global.cancelAnimationFrame = (id) => clearTimeout(id);
+        window.requestAnimationFrame = global.requestAnimationFrame;
+        window.cancelAnimationFrame = global.cancelAnimationFrame;
+    }
+
+    // Mock Canvas getContext for lottie-web
+    window.HTMLCanvasElement.prototype.getContext = function () {
+        return {
+            fillRect: function () {},
+            clearRect: function () {},
+            getImageData: function (x, y, w, h) {
+                return {
+                    data: new Array(w * h * 4)
+                };
+            },
+            putImageData: function () {},
+            createImageData: function () {
+                return [];
+            },
+            setTransform: function () {},
+            drawImage: function () {},
+            save: function () {},
+            restore: function () {},
+            beginPath: function () {},
+            moveTo: function () {},
+            lineTo: function () {},
+            closePath: function () {},
+            stroke: function () {},
+            translate: function () {},
+            scale: function () {},
+            rotate: function () {},
+            arc: function () {},
+            fill: function () {},
+            measureText: function () {
+                return {
+                    width: 0
+                };
+            },
+            transform: function () {},
+            rect: function () {},
+            clip: function () {},
+            
+            // properties
+            fillStyle: '',
+            strokeStyle: '',
+            lineWidth: 0,
+            lineCap: '',
+            lineJoin: '',
+            miterLimit: 0,
+            globalAlpha: 0,
+            canvas: this
+        };
+    };
 
 	// load the lottie renderer late after globals are set
 	const renderToDom = require("./render");
--- src/render.js
+++ src/render.js
@@ -13,6 +13,11 @@ function sanitizeShapes(shapes) {
 		if (shape.ty === 'gr' && !shape.it) {
 			shape.it = [];
 		}
+
+        // Remove dashes from strokes to prevent potential hangs
+        if (shape.ty === 'st' && shape.d) {
+            delete shape.d;
+        }
 		
 		// Recursively sanitize nested shapes
 		if (shape.it) {
@@ -48,12 +53,9 @@ function sanitizeAnimationData(data) {
 			}
 			
 			// Precomp layers (ty: 0) may have nested layers
-			if (layer.ty === 0 && layer.refId && sanitized.assets) {
-				const asset = sanitized.assets.find(a => a.id === layer.refId);
-				if (asset && asset.layers) {
-					sanitizeLayers(asset.layers);
-				}
-			}
+			// We don't need to recurse here because we iterate over all assets 
+            // in the main sanitizeAnimationData function loop below.
+            // Removing recursion avoids cycles and repeated work.
 		}
 	}
 	
@@ -80,8 +82,11 @@ module.exports = (document, animationData, opts, frameNumber) =>
 			const container = document.createElement("div");
 			document.body.append(container);
 
+            console.log("Starting sanitization...");
 			const safeAnimationData = sanitizeAnimationData(animationData);
+            console.log("Sanitization complete.");
 
+            console.log("Loading animation...");
 			var instance = lottie.loadAnimation({
 				container: container,
 				renderer: "svg",
@@ -90,10 +95,31 @@ module.exports = (document, animationData, opts, frameNumber) =>
 				animationData: safeAnimationData,
 				rendererSettings: opts
 			});
+            console.log("Animation loaded instance created.");
 
+			instance.addEventListener("config_ready", () => {
+				console.log("Lottie: config_ready");
+			});
+			instance.addEventListener("data_ready", () => {
+				console.log("Lottie: data_ready");
+			});
+			instance.addEventListener("data_failed", () => {
+				console.log("Lottie: data_failed");
+				reject(new Error("Lottie data failed to load"));
+			});
+			instance.addEventListener("error", (error) => {
+				console.log("Lottie: error", error);
+				reject(error);
+			});
 			instance.addEventListener("DOMLoaded", () => {
-				instance.goToAndStop(frameNumber, true);
-				resolve(container.innerHTML);
+				console.log("Lottie: DOMLoaded");
+				try {
+					instance.goToAndStop(frameNumber, true);
+					resolve(container.innerHTML);
+				} catch (e) {
+					console.error("Error during goToAndStop", e);
+					reject(e);
+				}
 			});
 		} catch (err) {
 			reject(err);
